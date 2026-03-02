84624be10d661143a38614e2fc9943921c17d4a1
Giulianno Sande <giulianno0013@gmail.com>
2026-03-02 13:57:55 -0300

    atualização

--- js/mobile.js
+++ js/mobile.js
@@ -5,3 +5,13 @@ menuToggle.addEventListener("click", () => {
   navLinks.classList.toggle("active");
   menuToggle.classList.toggle("open");
 });
+
+let logo = document.querySelector(".logo");
+
+function voltarPagina() {
+  if (history.length > 1) {
+    history.back();
+  } else {
+    window.location.href = "./";
+  }
+}
